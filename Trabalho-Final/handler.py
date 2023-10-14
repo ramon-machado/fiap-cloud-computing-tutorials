4788dd663334ec11af3e01165c096aad06f4dd32
Ramon Machado <rmorm5@gmail.com>
2023-10-14 23:41:31 +0000

    update

--- Trabalho-Final/handler.py
+++ Trabalho-Final/handler.py
@@ -4,12 +4,15 @@ from boto3.dynamodb.conditions import Key
 from baseDAO import BaseDAO
 
 
+dao = BaseDAO('eventos-pizzaria')
+
 def detailHandlerpizza(event, context):
     print("event: {}".format(json.dumps(event)))
     
     return True
     
 def sourceHandlerpizza(event, context):
-    print("event: {}".format(json.dumps(event)))
+    dao.put_item({'pedido':("pedido: {}".format(json.dumps(event)))})
+    #dao.put_item("event: {}".format(json.dumps(event)))
     
     return True
