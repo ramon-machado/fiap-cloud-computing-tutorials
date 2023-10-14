e0d796d1cc92fe6d1227b46abde2921e08ebbc0e
Ramon Machado <rmorm5@gmail.com>
2023-10-14 23:20:11 +0000

    update

--- Trabalho-Final/handler.py
+++ Trabalho-Final/handler.py
@@ -1,24 +1,12 @@
 import json
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+def detailHandlerpizza(event, context):
+    print("event: {}".format(json.dumps(event)))
+    
+    return True
+    
+def sourceHandlerpizza(event, context):
+    print("event: {}".format(json.dumps(event)))
+    
+    return True
