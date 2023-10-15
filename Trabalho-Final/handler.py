9c4943ce95623d59af7b91c3223962b2963f1e82
Ubuntu <ubuntu@ip-172-31-6-214.ec2.internal>
2023-10-15 20:15:29 +0000

    update

--- Trabalho-Final/handler.py
+++ Trabalho-Final/handler.py
@@ -2,15 +2,18 @@ import json
 import boto3
 from boto3.dynamodb.conditions import Key
 from baseDAO import BaseDAO
+from sqsHandler import SqsHandler
+from env import Variables
 
 
 dao = BaseDAO('eventos-pizzaria')
 
 def detailHandlerpizza(event, context):
-    print("event: {}".format(json.dumps(event)))
-    
-    return True
-    
+    env = Variables()
+    sqs = SqsHandler(env.get_sqs_url())
+    print(type(event))
+    sqs.send("event: {}".format(json.dumps(event)))
+        
 def sourceHandlerpizza(event, context):
     dao.put_item(
         {'pedido':(format(json.dumps(event['detail']['pedido']))),
@@ -19,3 +22,12 @@ def sourceHandlerpizza(event, context):
         'time':(format(json.dumps(event['time'])))}
     
     )
+    
+def consumerHandlerpizza(event, context):
+    env = Variables()
+    sqs = SqsHandler(env.get_sqs_url())
+    while(True):
+        response = sqs.getMessage(1)
+        if(len(response) == 0):
+            break
+        print(response)
